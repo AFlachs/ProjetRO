269f679032a4bd8969ed9e4ea0741c527a923a43
Dubus-Benoit <83063797+Dubus-Benoit@users.noreply.github.com>
2021-04-30 11:21:25 +0200

    Youhou
    Le pseudo code de comment modéliser la quantité

---
+++ modelQuantité.cpp
@@ -0,0 +1,48 @@
+#include "library_containing_all_I_want"
+
+/* /!\Anvers dernière est la dernière ville */
+
+double q_v,s=q_v,s-1;
+for (j in semestre){
+	for camion type 1{
+		for fois{
+			if (P==0){
+				for vin V{
+					if (x_v){
+						q_v+=16.5;
+						break;
+					}
+				}
+			} else {
+				if (x_anvers){
+					q_liège+=16.5;
+				}
+			}
+		}
+	for camion type 2{
+		for fois{
+			if (P==0) {
+				for vin V{
+					if (x_v){
+						q_v+=16.5;
+						break;
+					}
+				}
+				if (x_Anvers){
+					q_iège+=5.5;
+				}
+			} else {
+				for vin V{
+					if (x_v){
+						q_v+=5.5;
+						break;
+					}
+				}
+				if (x_Anvers){
+					q_liège+=16.5;
+				}
+			}
+		}
+	}
+}
+q_v,s>=contrainte
