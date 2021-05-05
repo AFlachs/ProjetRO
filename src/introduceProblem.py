3d819b34b79f41421554cfdea6e113b061642d2b
Alexandre Flachs <alexandre.flachs@ulb.be>
2021-05-05 18:59:59 +0200

    Reformat de fichier

--- src/introduceProblem.py
+++ src/introduceProblem.py
@@ -45,7 +45,7 @@ def introduce_city_requests():
 
 def introduce_semesters():
     semesters = np.array(
-        [0,1,2,3,4,5,6,7,8,9,10]
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     )
     return semesters
 
