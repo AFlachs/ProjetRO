1d81f427dfd279cf58fe58f9003d216aeeefc429
Alexandre Flachs <alexandre.flachs@ulb.be>
2021-05-05 18:47:05 +0200

    En cours d'écriture des coûts

--- src/costs.py
+++ src/costs.py
@@ -1,11 +1,26 @@
-
-def salary(x):
+def distances_camion(x, y, distances, c, j):
+    """
+    Distance parcourue par un camion c le jour j
+    """
+    first_part = sum(2)
     return 1
 
 
-def maintainance(x):
-    return 1
+def salary(x, y, distances, V_moy):
+    alpha = 13  # Prix de l'heure
+    return alpha * (sum(
+        distances_camion(x, y, distances, c, j) / V_moy for c in range(len(x)) for j in range(len(x[0][0][0])))
+                    + sum(x[c][f][v][j]
+                          for c in range(len(x))
+                          for f in range(len(x[0]))
+                          for v in range(len(x[0][0]))
+                          for j in range(len(x[0][0][0]))))
 
 
-def fuel(x):
-    pass
+def maintainance(n):
+    return 2000*n
+
+
+def fuel(x, distances):
+    l = 0.35  # consommation d'essence par kilomètre
+    c_e = 1.5 # Prix du litre
--- src/main.py
+++ src/main.py
@@ -16,7 +16,9 @@ max_capacity_1 = 16.5   # Tonnes
 max_capacity_2 = 5.5    # Tonnes
 
 
-x = list(list(list(list)))   # x_vj^cf -> camion passe ou pas
+# TODO : introduce variables
+x = list(list(list(list)))   # x_vj^cf -> camion passe ou pas, x[c][f][v][j]
+y = list(list(list(list)))   # y_vj^cf -> produit x_v * x_a
 p = list(list)               # p_cj -> type transporté
 n = list()                   # n_s -> nombre de camions au semestre s
 
@@ -33,6 +35,15 @@ model += (2*x1 + x2 + x3 <= 20, 'Constraint 1')
 model += (3*x1 + x2 + 2*x3 <= 30, 'Constraint 2')
 
 
-model += costs.salary(x) + costs.maintainance(x) + costs.fuel(x), 'Objective Function'
+for c in range(len(y)):
+    for f in range(len(y[0])):
+        for v in range(len(y[0][0])):
+            for j in range(len(y[0][0][0]))):
+                model += (y[c][f][v][j] <= x[c][f][v][j], 'Produit de binaires')
+                model += (y[c][f][v][j] <= x[c][f][len(x)-1][j], 'Produit de binaires')  # indice d'anvers
+                model += (y[c][f][v][j] >= x[v] + x[len(x)-1] - 1, 'Produit de binaires')
+
+
+model += costs.salary(x, distances) + costs.maintainance(x) + costs.fuel(x), 'Objective Function'
 
 status = model.solve(solver=GLPK(msg=True, keepFiles=True))
