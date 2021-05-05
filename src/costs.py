dfbe7f9def0854459d21c69576affa52279b1bdc
Alexandre Flachs <alexandre.flachs@ulb.be>
2021-05-05 18:56:35 +0200

    Couts du au salaire

--- src/costs.py
+++ src/costs.py
@@ -2,25 +2,34 @@ def distances_camion(x, y, distances, c, j):
     """
     Distance parcourue par un camion c le jour j
     """
-    first_part = sum(2)
-    return 1
+    first_part = sum(2 * x[c][f][v][j] * distances[v][len(distances)]
+                     for f in range(len(x[0]))
+                     for v in range(len(x[0][0]))
+                     )
+    second_part = sum(y[c][f][v][j] * (distances[v][0] + distances[0][len(distances)] - distances[v][len(distances)])
+                      for f in range(len(x[0]))
+                      for v in range(len(x[0][0]))
+                      )
+    return first_part + second_part
 
 
 def salary(x, y, distances, V_moy):
-    alpha = 13  # Prix de l'heure
-    return alpha * (sum(
-        distances_camion(x, y, distances, c, j) / V_moy for c in range(len(x)) for j in range(len(x[0][0][0])))
-                    + sum(x[c][f][v][j]
-                          for c in range(len(x))
-                          for f in range(len(x[0]))
-                          for v in range(len(x[0][0]))
-                          for j in range(len(x[0][0][0]))))
+    alpha = 13  # Paie par l'heure
+    return alpha * (
+            sum(distances_camion(x, y, distances, c, j) / V_moy for c in range(len(x)) for j in range(len(x[0][0][0])))
+            + sum(x[c][f][v][j]
+                  for c in range(len(x))
+                  for f in range(len(x[0]))
+                  for v in range(len(x[0][0]))
+                  for j in range(len(x[0][0][0]))
+                  )
+    )
 
 
 def maintainance(n):
-    return 2000*n
+    return 2000 * n
 
 
 def fuel(x, distances):
     l = 0.35  # consommation d'essence par kilomètre
-    c_e = 1.5 # Prix du litre
+    c_e = 1.5  # Prix du litre
